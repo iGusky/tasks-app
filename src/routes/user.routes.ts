1b0a53f3f0070f7bdcba784cd60cf15b3c56d3da
Gustavo Hernández <gustavohernandez.dev@gmail.com>
2026-01-02 16:53:42 -0600

    feat: show user profile information

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, profile } from "../controllers/user.controller.js";
 
 const router = Router()
 
 router.post("/", createUser)
+router.get("/profile", profile)
 
 export default router
